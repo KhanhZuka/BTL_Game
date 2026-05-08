a9351daa8a56c7061023a3037fc82902206f63f0
ahny143 <vutuananh1st@gmail.com>
2026-05-09 00:30:16 +0700

    Hoàn thiện AttackUp_Down và Portal chuyển màn

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public string sceneName;
+
+    bool playerInRange;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        playerInRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        playerInRange = false;
+    }
+
+    void Update()
+    {
+        // Nhấn F khi đang đứng trong portal
+        if (playerInRange && Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
